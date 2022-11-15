97674f11e5ecdcf0d28df0a6684bd743bb1cb595
DarioSantosSousa <dariovsgeovanna@gmail.com>
2022-11-15 11:32:16 -0300

    SHIFT + ALT + F

--- App.js
+++ App.js
@@ -44,7 +44,7 @@ export default function App() {
     }
     switch (buttonPressed) {
       case 'DEL':
-        setCurrentNumber(currentNumber.substring(0, (currentNumber.length -1)))
+        setCurrentNumber(currentNumber.substring(0, (currentNumber.length - 1)))
         return
       case 'LIMPAR': // Limpa todo o conteúdo
         setLastNumber("")
@@ -85,48 +85,48 @@ export default function App() {
             )}
           </View>
         </View>
-        </View>
-        </View>
-        );
+      </View>
+    </View>
+  );
 }
 
-        // Estilização
-        const styles = StyleSheet.create({
-          container: {
-          flex: 1,
+// Estilização
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
   },
-        results: {
-          flex: 2,
-        justifyContent: "center",
-        backgroundColor: "#f5f5f5"
+  results: {
+    flex: 2,
+    justifyContent: "center",
+    backgroundColor: "#f5f5f5"
   },
-        resultText: {
-          color: "#282F38",
-        fontSize: 32,
-        fontWeight: "bold",
-        padding: 12,
-        textAlign: "right"
+  resultText: {
+    color: "#282F38",
+    fontSize: 32,
+    fontWeight: "bold",
+    padding: 12,
+    textAlign: "right"
   },
-        historyText: {
-          color: "#7c7c7c",
-        fontSize: 20,
-        marginRight: 10,
-        alignSelf: 'flex-end',
+  historyText: {
+    color: "#7c7c7c",
+    fontSize: 20,
+    marginRight: 10,
+    alignSelf: 'flex-end',
   },
-        buttons: {
-          flexDirection: 'row',
-        flexWrap: 'wrap',
+  buttons: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
   },
-        button: {
-          backgroundColor: 'white',
-        alignItems: 'center',
-        justifyContent: 'center',
-        minWidth: 90,
-        minHeight: 90,
-        flex: 2,
+  button: {
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+    minWidth: 90,
+    minHeight: 90,
+    flex: 2,
   },
-        textButton: {
-          color: "#7c7c7c",
-        fontSize: 20,
+  textButton: {
+    color: "#7c7c7c",
+    fontSize: 20,
   }
 });
